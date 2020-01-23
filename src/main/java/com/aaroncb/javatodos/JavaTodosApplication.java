9caea26f2c23d8bca0bdc0adf076d4b017b713a0
MaciXB <aaroncb@aaroncb.com>
2020-01-23 12:06:19 -0600

    Initial Commit

---
+++ src/main/java/com/aaroncb/javatodos/JavaTodosApplication.java
@@ -0,0 +1,13 @@
+package com.aaroncb.javatodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTodosApplication.class, args);
+    }
+
+}
