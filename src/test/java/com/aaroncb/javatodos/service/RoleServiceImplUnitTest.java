cfb3f5ea71dbbe76bf23e254aa5019d69082ed07
Aaron Beasley <aaroncb@aaroncb.com>
2020-01-26 00:58:20 -0600

    ServiceImpl Testing
    - Added RoleServiceImpl basic unit test's based on SeedData
    - Added TodoServiceImpl basic unit test's based on SeedData
    - Added UserServiceImpl basic unit test's based on SeedData

--- src/test/java/com/aaroncb/javatodos/service/RoleServiceImplUnitTest.java
+++ src/test/java/com/aaroncb/javatodos/service/RoleServiceImplUnitTest.java
@@ -8,6 +8,60 @@
 
 package com.aaroncb.javatodos.service;
 
-public class RoleServiceImplUnitTest {
+import com.aaroncb.javatodos.JavaTodosApplicationTests;
+import com.aaroncb.javatodos.models.Role;
+import com.aaroncb.javatodos.services.RoleService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JavaTodosApplicationTests.class)
+
+public class RoleServiceImplUnitTest
+{
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Transactional
+    @Test
+    public void findAllRoles() throws Exception
+    {
+        List<Role> roleList =  roleService.findAll();
+
+        assertEquals(3, roleList.size());
+
+        for (Role r : roleList)
+        {
+            // System.out.println(r);
+            System.out.println(mapper.writeValueAsString(r));
+        }
+    }
 }
--- src/test/java/com/aaroncb/javatodos/service/TodoServiceImplUnitTest.java
+++ src/test/java/com/aaroncb/javatodos/service/TodoServiceImplUnitTest.java
@@ -8,6 +8,59 @@
 
 package com.aaroncb.javatodos.service;
 
-public class TodoServiceImplUnitTest {
+import com.aaroncb.javatodos.JavaTodosApplicationTests;
+import com.aaroncb.javatodos.models.Todo;
+import com.aaroncb.javatodos.services.TodoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JavaTodosApplicationTests.class)
+public class TodoServiceImplUnitTest
+{
+    @Autowired
+    private TodoService todoService;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Transactional
+    @Test
+    public void findAllTodos() throws Exception
+    {
+        List<Todo> todoList =  todoService.findAll();
+
+        assertEquals(304, todoList.size());
+
+        for (Todo t : todoList)
+        {
+            // System.out.println(r);
+            System.out.println(mapper.writeValueAsString(t));
+        }
+    }
 }
